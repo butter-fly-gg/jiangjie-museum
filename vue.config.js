3b11edb8a74f5c26e374c82f4a4c7eb38d058b53
root <256739203+lcr214@users.noreply.github.com>
2026-04-24 23:31:50 +0800

    添加.gitignore，移除node_modules追踪，新增预约功能

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  // --- 添加 configureWebpack 配置 ---
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        "http": require.resolve("stream-http"),
+        "https": require.resolve("https-browserify"),
+        "stream": require.resolve("stream-browserify"),
+        "url": require.resolve("url/"),
+        "assert": require.resolve("assert/"),
+        "util": require.resolve("util/"),
+        "zlib": require.resolve("browserify-zlib"),
+        "path": require.resolve("path-browserify"),
+        "crypto": require.resolve("crypto-browserify")
+      }
+    }
+  }
 })
