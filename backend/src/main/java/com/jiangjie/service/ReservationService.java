239e81a2959855cdbe0ebab5b23f808f2c648f18
熊珂 <3417490652@qq.com>
2026-04-29 23:08:35 +0800

    修改bug

--- backend/src/main/java/com/jiangjie/service/ReservationService.java
+++ backend/src/main/java/com/jiangjie/service/ReservationService.java
@@ -1,8 +1,11 @@
 package com.jiangjie.service;
 
-import com.baomidou.mybatisplus.extension.service.IService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.jiangjie.entity.Reservation;
+import com.jiangjie.mapper.ReservationMapper;
+import org.springframework.stereotype.Service;
 
-public interface ReservationService extends IService<Reservation> {
-    // 如果需要复杂的业务逻辑（比如检查库存、生成核销码），可以在这里定义方法
+@Service
+public class ReservationService extends ServiceImpl<ReservationMapper, Reservation> {
+    // 里面什么都不用写，和其他Service保持一致
 }
